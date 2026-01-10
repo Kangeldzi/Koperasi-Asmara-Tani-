33c863769d7c6edab061ad8f31ac1526d4ec4974
Kang L <143698732+Kangeldzi@users.noreply.github.com>
2026-01-10 20:19:44 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+‎  apiKey: "AIzaSyCfvnFflFgcDejLnq8nywi5giXqwNy3yuU",
+‎  authDomain: "koperasi-asmara-tani.firebaseapp.com",
+‎  projectId: "koperasi-asmara-tani",
+‎  storageBucket: "koperasi-asmara-tani.firebasestorage.app",
+‎  messagingSenderId: "1025760764894",
+‎  appId: "1:1025760764894:web:7f0e6621d50cddb1adf34f",
+‎  measurementId: "G-P0H3ZX4EFK"
+‎};
